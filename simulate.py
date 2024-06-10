5b3650600a015f268c3a7bcd3c44fc82a545fe9f
maxsivry <mivry@uvm.edu>
2024-06-10 17:21:06 -0500

    changes

--- simulate.py
+++ simulate.py
@@ -1,3 +1,10 @@
+import time as t
 import pybullet as p
 physicsClient = p.connect(p.GUI)
+
+# to disable the sidebar uncomment following line
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+for i in range(1000):
+    p.stepSimulation()
+    t.sleep(1/60)
 p.disconnect()
